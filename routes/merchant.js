d456bfa18e9624a2488fb53b409089c266385055
Chris Wen <s224212855@deakin.edu.au>
2025-01-16 21:44:16 +1100

    update dev branch

--- routes/merchant.js
+++ routes/merchant.js
@@ -18,39 +18,6 @@ const {
   handleCreateBooking,
 } = require("../controllers/BookingController");
 
-// router.get("/:merchantId", async function (req, res) {
-//   let merchantId = req.params.merchantId;
-//   let merchant = await getMerchantByMerchantId(merchantId);
-const {
-  getMerchantByMerchantId,
-  renderCreateMerchant,
-  handleCreateMerchant,
-  renderMerchantDetails,
-  renderCreateMerchantPhotoGallery,
-  handleCreateMerchantPhotoGallery,
-  renderCreateMerchantOpenHours,
-  handleCreateMerchantOpenHours,
-} = require("../controllers/MerchantController");
-const { getMenuByMerchantId } = require("../controllers/MenuController");
-
-const {
-  renderCreateBooking,
-  handleCreateBooking,
-} = require("../controllers/BookingController");
-
-// router.get("/:merchantId", async function (req, res) {
-//   let merchantId = req.params.merchantId;
-//   let merchant = await getMerchantByMerchantId(merchantId);
-
-//   res.render("./merchant/merchant", {
-//     merchant: merchant,
-//   });
-// });
-//   res.render("./merchant/merchant", {
-//     merchant: merchant,
-//   });
-// });
-
 router.get('/register',async function (req,res) {
   res.render('./merchant/register',{
       
@@ -70,9 +37,7 @@ router.get("/:merchantId/menu", async function (req, res) {
   res.render("./menu/menu", {
     menu: menu,
   });
-  res.render("./menu/menu", {
-    menu: menu,
-  });
+
 });
 
 // Render a merchant details routes
@@ -99,23 +64,6 @@ router.post("/:merchantId/hours", handleCreateMerchantOpenHours);
 // Render a merchant details routes
 router.get("/:merchantId", renderMerchantDetails);
 
-// Render create merchant routes
-router.get("/", renderCreateMerchant);
-
-// handle create merchant routes
-router.post("/", handleCreateMerchant);
-
-// Render create merchant photogallery routes
-router.get("/:merchantId/photo", renderCreateMerchantPhotoGallery);
-
-// handle create merchant photogallery routes
-router.post("/:merchantId/photo", handleCreateMerchantPhotoGallery);
-
-// Render create merchant photogallery routes
-router.get("/:merchantId/hours", renderCreateMerchantOpenHours);
-
-// handle create merchant photogallery routes
-router.post("/:merchantId/hours", handleCreateMerchantOpenHours);
 
 module.exports = router;
 
