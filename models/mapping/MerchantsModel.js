f0251cb32cf0058797eb474d000f0f9343eda47b
Dhian <muthidp@gmail.com>
2025-01-24 13:17:00 +1100

    Merchant model

--- models/mapping/MerchantsModel.js
+++ models/mapping/MerchantsModel.js
@@ -9,10 +9,39 @@ const MerchantsSchema = new mongoose.Schema(
     type: { type: types.String, required: true },
     description: { type: types.String, required: true },
     location: { type: types.String, required: true },
+    // @TODO: location schema for later
+    // location: {
+    //   address: { type: types.String, required: true },
+    //   suburb: { type: types.String, required: true },
+    //   state: { type: types.String, required: true },
+    //   postCode: { type: types.Number, required: true },
+    //   lat: { type: types.Number },
+    //   long: { type: types.Number },
+    // }, 
+    openHours: [
+      {
+        day: {
+          type: String,
+          enum: [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+          ], // Days of the week
+          required: true
+        },
+        slots: [
+          { time: { type: String, required: true, match: /^([01]\d|2[0-3]):([0-5]\d)$/ } }, // 24-hour format (e.g., 09:00)
+          { availableSlots: { type: String, required: true, min: 0 } }
+        ]
+      }
+    ],
     contactPhone: { type: types.String, required: true },
     hours: { type: types.String, required: true },
     photoGallery: { type: types.Array, required: true },
-    openHours: { type: types.Array, required: true },
     password: { type: types.String, required: true },
     isDeleted: { type: types.Boolean, required: true, default: false },
     star: { type: types.Number },
