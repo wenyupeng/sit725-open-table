50cf08862d1bf8a6c42781cc0e9f82d1d2ed0138
Dhian <muthidp@gmail.com>
2025-02-03 10:48:09 +1100

    Fix menu

--- config/db.config.js
+++ config/db.config.js
@@ -10,6 +10,6 @@
 
 module.exports = {
     port: process.env.DB_PORT,
-    url: `mongodb://localhost:27017/skippy`
+    url: process.env.MONGODB_URI
     // mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}
 }
